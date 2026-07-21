b523c63f6b29e73516879625b17def2f53bb313f
ST4RGUARD <thema11ock@gmail.com>
2026-07-21 16:43:44 -0500

    add comments and clear

--- lua/omnivision/commands.lua
+++ lua/omnivision/commands.lua
@@ -16,10 +16,28 @@ local function hello()
 	require("omnivision").hello()
 end
 
+local extmarks = require("omnivision.core.extmarks")
+
+local function test_virtual_text()
+	local buf = vim.api.nvim_get_current_buf()
+	local line = vim.api.nvim_win_get_cursor(0)[1] - 1
+
+	extmarks.show(buf, line, "=> OmniVision works!")
+end
+
+local function clear()
+	extmarks.clear_all()
+	vim.notify("OmniVision cleared")
+end
+
 function M.setup()
 	vim.api.nvim_create_user_command("OmniVision", hello, {})
 
 	vim.api.nvim_create_user_command("OmniVisionReload", reload, {})
+
+	vim.api.nvim_create_user_command("OmniVisionTest", test_virtual_text, {})
+
+	vim.api.nvim_create_user_command("OmniVisionClear", clear, {})
 end
 
 M.setup()
---
+++ lua/omnivision/core/extmarks.lua
@@ -0,0 +1,24 @@
+local M = {}
+
+local namespace = vim.api.nvim_create_namespace("omnivision")
+
+function M.show(bufnr, line, text)
+	vim.api.nvim_buf_set_extmark(bufnr, namespace, line, -1, {
+		virt_text = {
+			{ text, "Comment" },
+		},
+		virt_text_pos = "eol",
+	})
+end
+
+function M.clear(bufnr)
+	vim.api.nvim_buf_clear_namespace(bufnr, namespace, 0, -1)
+end
+
+function M.clear_all()
+	for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+		M.clear(buf)
+	end
+end
+
+return M
